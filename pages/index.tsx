07a79716b5602ce20b28954ecb4af838a85137ac
azizkhoso <azizkhoso586@gmail.com>
2023-03-13 23:01:12 +0500

    feat: setup hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import { Box, Button, Container, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Text, Image as ChImage } from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
+
+import halfProfile from '../public/half-profile.png';
+import reactIcon from '../public/react-icon.svg';
+import nodejsIcon from '../public/nodejs-icon.svg';
+import mongodbIcon from '../public/mongodb-icon.svg';
+import verifiedIcon from '../public/verified-icon.svg';
+import completeProject from '../public/complete-project.svg';
 
 import Navbar from '../components/Navbar';
 import SvgShortArrow from '../components/Svg/SvgShortArrow';
@@ -10,7 +17,15 @@ export default function Home() {
     <>
       <Navbar />
       <Box bgColor="black">
-        <Box as={Container} maxW="container.xl" display="flex" flexFlow="row wrap" px={0}>
+        <Box
+          as={Container}
+          maxW="container.2xl"
+          pb="16"
+          pt="36"
+          justifyContent="space-between"
+          display="flex"
+          flexFlow="row wrap"
+        >
           <Flex
             w={{ base: 'full', lg: '48%' }}
             flexDir="column"
@@ -48,6 +63,56 @@ export default function Home() {
             >
               Hire me
             </Button>
+          </Flex>
+          <Flex
+            w={{ base: 'full', lg: '48%' }}
+            flexDir="column"
+            alignItems={{ base: 'center', lg: 'flex-start' }}
+            textAlign={{ base: 'center', lg: 'left' }}
+          >
+            <Box width="600px" height="600px" pos="relative">
+              {[
+                { icon: reactIcon, top: '10%', left: '0' },
+                { icon: nodejsIcon, top: '10%', right: '0' },
+                { icon: mongodbIcon, bottom: '10%', right: '0' },
+                { icon: completeProject, bottom: '10%', left: '0', isDifferent: true },
+              ].map((item, index) => (
+                <Box
+                  key={index}
+                  borderRadius={24}
+                  width={item.isDifferent ? 'auto' : '100px'}
+                  height={item.isDifferent ? 'auto' : '100px'}
+                  bgColor="white"
+                  pos="absolute"
+                  top={item.top}
+                  left={item.left}
+                  right={item.right}
+                  bottom={item.bottom}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Image
+                    src={item.icon}
+                    alt="reactjs"
+                    style={{
+                      width: '100%',
+                      maxWidth: item.isDifferent ? '200px' : '70px',
+                      backgroundColor: item.isDifferent ? 'white' : '#002742',
+                      borderRadius: '16px',
+                      padding: '12px',
+                    }}
+                  />
+                </Box>
+              ))}
+              <Image
+                src={halfProfile.src}
+                width={600}
+                height={600}
+                style={{ borderRadius: '50%', backgroundColor: '#4fc7ea' }}
+                alt="half-profile"
+              />
+            </Box>
           </Flex>
         </Box>
       </Box>
