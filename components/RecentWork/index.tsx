24e77ce0af9bdc056dc583113e3a58ee33bbc0e4
azizkhoso <azizkhoso586@gmail.com>
2025-08-12 11:11:51 +0500

    feat: add projects pics in recent work

--- components/RecentWork/index.tsx
+++ components/RecentWork/index.tsx
@@ -9,9 +9,9 @@ import SvgShortArrow from '../Svg/SvgShortArrow';
 import SvgWinner from '../Svg/SvgWinner';
 
 const projects = [
-  { name: 'Zoxxo', category: 'Web App', image: 'https://dummyimage.com/1200x1200/000/fff&text=Zoxxo' },
-  { name: 'Jebreal', category: 'Web App', image: 'https://dummyimage.com/1200x1200/000/fff&text=Jebreal' },
-  { name: 'Grade My Faculty', category: 'Web App', image: 'https://dummyimage.com/1200x1200/000/fff&text=Grade My Faculty' }
+  { name: 'Zoxxo', category: 'Web App', image: '/assets/zoxxo-cover.png' },
+  { name: 'Jebreal', category: 'Web App', image: '/assets/jebreal-cover.png' },
+  { name: 'Grade My Faculty', category: 'Web App', image: '/assets/gmf-cover.png' }
 ]
 
 function SimpleSlider() {
@@ -56,23 +56,25 @@ function SimpleSlider() {
   };
   return (
     <Box display="flex" flexDir="column">
-      <Slider ref={(s) => {if (s) sliderRef.current = s;}} {...settings}>
+      <Slider ref={(s) => { if (s) sliderRef.current = s; }} {...settings}>
         {projects.map((proj) => (
-          <Flex key={proj.name} flexDir="column" w="full" border="2px solid gray" px="2">
-            <Box w="full" minH="400px" overflow="hidden">
-              <Img src={proj.image} _hover={{ transform: 'scale(1.2)' }} />
-            </Box>
-            <Heading as="h4" mt="7" fontSize="xl">{proj.name}</Heading>
-            <Text mt="2" fontSize="sm">{proj.category}</Text>
+          <Flex key={proj.name} flexDir="column" w="full" px="2">
+            <Flex flexDir="column" w="full" backgroundColor="rgba(255, 255, 255, 0.2)" borderRadius="9px">
+              <Box w="full" minH="400px" overflow="hidden">
+                <Img src={proj.image} _hover={{ transform: 'scale(1.2)' }} />
+              </Box>
+              <Heading as="h4" mt="7" fontSize="xl" px="4">{proj.name}</Heading>
+              <Text my="2" fontSize="sm" px="4">{proj.category}</Text>
+            </Flex>
           </Flex>
         ))}
       </Slider>
       <Flex w="full" alignItems="center" py="4" gap="8px" justifyContent="center">
-        <Button variant="simple" rounded="full" sx={{padding: 0, justifyContent: 'center'}} onClick={prev}>
-          <Box as={SvgShortArrow} sx={{path: {fill: 'white'}, transform: 'rotate(-90deg)', aspectRatio: '1'}} padding={0} />
+        <Button variant="simple" rounded="full" sx={{ padding: 0, justifyContent: 'center' }} onClick={prev}>
+          <Box as={SvgShortArrow} sx={{ path: { fill: 'white' }, transform: 'rotate(-90deg)', aspectRatio: '1' }} padding={0} />
         </Button>
-        <Button variant="simple" rounded="full" sx={{padding: 0, justifyContent: 'center'}} onClick={next}>
-          <Box as={SvgShortArrow} sx={{path: {fill: 'white'}, transform: 'rotate(90deg)', aspectRatio: '1'}} padding={0} />
+        <Button variant="simple" rounded="full" sx={{ padding: 0, justifyContent: 'center' }} onClick={next}>
+          <Box as={SvgShortArrow} sx={{ path: { fill: 'white' }, transform: 'rotate(90deg)', aspectRatio: '1' }} padding={0} />
         </Button>
       </Flex>
     </Box>
@@ -90,14 +92,14 @@ export default function RecentWork() {
         alignItems="center"
         py="10"
       >
-        <Flex flexDir={{base: 'column', md: 'row'}} alignItems="center" gap="28px">
+        <Flex flexDir={{ base: 'column', md: 'row' }} alignItems="center" gap="28px">
           <Flex flexDir="column">
             <Heading as="h2" fontWeight={700} fontSize="5xl" my="5">Recent Work</Heading>
             <Text maxW={{ base: '100%', md: '60%', lg: '50%' }} color="#d8d8d8" lineHeight="7">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae temporibus illum consequatur magnam sed. Dolor repudiandae quasi, cupiditate, libero ipsum debitis iure modi dolorum hic iste illo ea impedit aperiam.
             </Text>
           </Flex>
-          <Box minH="100px" h="80%" sx={{aspectRatio: '1'}} backgroundColor="#282828" borderRadius="50%" display="flex" alignItems="center" justifyContent="center">
+          <Box minH="100px" h="80%" sx={{ aspectRatio: '1' }} backgroundColor="#282828" borderRadius="50%" display="flex" alignItems="center" justifyContent="center">
             <SvgWinner width="60px" height="60px" />
           </Box>
         </Flex>
