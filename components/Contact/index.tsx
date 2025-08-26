6ff5aeac97fbfbcec70b7c82091220bd7fc765f1
azizkhoso <azizkhoso586@gmail.com>
2025-08-26 20:09:07 +0500

    feat: update contact section along with new icons

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,7 +1,10 @@
-import { Box, Container, Flex, Heading, Text, Image, VStack, FormControl, FormLabel, Input, Textarea, Stack, Icon } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, Text, Image, VStack, FormControl, FormLabel, Input, Textarea, Stack, Icon, Link } from '@chakra-ui/react';
 import SvgShortArrow from '../Svg/SvgShortArrow';
 import Button from '../common/Button';
 import SvgMenuBars from '../Svg/SvgMenuBars';
+import SvgEmail from '../Svg/SvgEmail';
+import SvgLinkedIn from '../Svg/SvgLinkedIn';
+import SvgGitHub from '../Svg/SvgGitHub';
 
 export default function Contact() {
   return (
@@ -14,17 +17,20 @@ export default function Contact() {
         alignItems="center"
         py="10"
       >
-        <Heading as="h2" fontWeight={700} textAlign="center" fontSize={{ base: '2xl', md: '3xl', lg: '5xl' }} my="5">Let's Talk</Heading>
+        <Heading as="h2" fontWeight={700} textAlign="center" fontSize={{ base: '2xl', md: '3xl', lg: '5xl' }} my="5">Let&apos;s Talk</Heading>
         <Text maxW={{ base: '100%', md: '60%', lg: '50%' }} color="#d8d8d8" lineHeight="7" textAlign="center" fontSize={{ base: 'sm', lg: 'md' }}>
-          Need your project fixed or built? I am just a click away. Reach out to me via email or LinkedIn and let's discuss how I can help you achieve your goals.
+          Need your project fixed or built? I am just a click away. Reach out to me via email or LinkedIn and let&apos;s discuss how I can help you achieve your goals.
         </Text>
         <Flex
           flexDir="row"
           flexWrap="wrap"
-          gap="12px"
+          rowGap={8}
+          columnGap={8}
           mt="12"
+          w="full"
+          justifyContent="space-between"
         >
-          <Stack spacing={4} w={{ base: '100%', md: '80%', lg: '60%' }} alignItems="center" p="8" bgColor="gray.900" border="1px solid gray.900" borderRadius={8}>
+          <Stack spacing={4} w={{ base: '100%', lg: '58%' }} alignItems="center" p="8" bgColor="gray.900" border="1px solid gray.900" borderRadius={8}>
             <Heading as="h4">Get In Touch</Heading>
             <Flex as="form" flexDir="column" gap="4" w="full">
               <FormControl>
@@ -46,16 +52,22 @@ export default function Contact() {
             </Flex>
             <Button type='submit' variant="rounded">Submit</Button>
           </Stack>
-          <Flex flexDir="column">
-            <Stack bgColor="gray.900" p="8" border="1px solid gray.900" borderRadius={8} spacing={2}>
-              <Heading as="h6" fontSize="2xl">Email</Heading>
-              <Box display="flex" alignItems="center" justifyContent="center" gap="4">
-                <Box display="flex" alignItems="center" justifyContent="center" rounded="full" bgColor="gray.700" p="4">
-                  <Icon as={SvgMenuBars} boxSize="6" sx={{'& path': { fill: 'yellow'}}} />
+          <Flex flexDir="column" w={{base: '100%', lg: '38%'}} rowGap="8">
+            {[
+              { title: 'Email', icon: SvgEmail, text: 'azizkhoso586@gmail.com', link: 'mailto:azizkhoso586@gmail.com' },
+              { title: 'Github', icon: SvgGitHub, text: 'azizkhoso', link: 'https://www.github.com/azizkhoso' },
+              { title: 'LinkedIn', icon: SvgLinkedIn, text: 'aziz-khoso', link: 'https://www.linkedin.com/in/aziz-khoso/' },
+            ].map((item) => (
+              <Stack key={item.title} bgColor="gray.900" p="8" border="1px solid gray.900" borderRadius={8} spacing={2}>
+                <Heading as="h6" fontSize="2xl">{item.title}</Heading>
+                <Box display="flex" alignItems="center" gap="4">
+                  <Box display="flex" alignItems="center" justifyContent="center" rounded="full" bgColor="gray.700" p="4">
+                    <Icon as={item.icon} boxSize="8" sx={{ '& path': { fill: 'yellow' } }} />
+                  </Box>
+                  <Link href={item.link} target="_blank" fontSize="md">{item.text}</Link>
                 </Box>
-                <Text fontSize="md">azizkhoso586@gmail.com</Text>
-              </Box>
-            </Stack>
+              </Stack>
+            ))}
           </Flex>
         </Flex>
       </Container>
